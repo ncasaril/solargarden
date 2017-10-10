44e5e6d370b351831dd29fabd79dde9a19001e38
Niklas Casaril <niklas@casaril.com>
2017-10-10 14:33:53 +1000

    crudely working

--- atmega/atmega.cpp
+++ atmega/atmega.cpp
@@ -8,14 +8,24 @@
     #include "WProgram.h"
 #endif
 
-const int SOLAR_INPUT_ADC_PIN    =A0;
-const int BATTERY_ADC_PIN =A1;
-const int LIGHT0_ENABLE_PIN      =2;
+const int SOLAR_INPUT_ADC_PIN    = A0;
+const int BATTERY_ADC_PIN        = A1;
+const int LIGHT0_ENABLE_PIN      =  2;
 
-const float SOLAR_ACTIVE_LIMIT    =20.0;
-const float BATTERY_POWER_LIMIT   =3.2*5;
+const float SOLAR_ACTIVE_LIMIT    = 25.0;
+const float SOLAR_V_CAL           = 25.0/18.13;
+const float BATTERY_POWER_LIMIT   = 3.2*5;
+const float BATTERY_V_CAL         = 20.6/15.70;
 
-const float FILTER_FACTOR = 0.1;
+const float FILTER_FACTOR = 0.01;
+const int  MID_LIGHT = 16;
+const int  MAX_LIGHT = 64;
+const int  LIGHT_INCREMENT = 1;
+
+uint8_t _n=0;
+uint16_t _light0_intensity = 0;
+float _solar = 0;
+float _battery = 0;
 
 void setup() 
 {
@@ -23,6 +33,9 @@ void setup()
     Serial.println(F("Solarctrl Setup"));
 
     analogReference(DEFAULT);    
+    //analogReference(EXTERNAL);    
+    digitalWrite(SOLAR_INPUT_ADC_PIN, LOW);
+    digitalWrite(BATTERY_ADC_PIN, LOW);
     pinMode(SOLAR_INPUT_ADC_PIN, INPUT);
     pinMode(BATTERY_ADC_PIN, INPUT);
     
@@ -31,35 +44,34 @@ void setup()
     
     Serial.println(F(""));
     Serial.println(F("Setup done"));
+
+    _solar = analogRead(SOLAR_INPUT_ADC_PIN)/1023.0 * 50.0 * SOLAR_V_CAL;
+    _battery = analogRead(BATTERY_ADC_PIN)/1023.0 * 50.0 * BATTERY_V_CAL;
 }
 
-uint8_t _n=0;
-uint16_t _light0_intensity = 0;
-float _solar = 0;
-float _battery = 0;
 void loop() 
 {
-    float solar = analogRead(SOLAR_INPUT_ADC_PIN)  * 50/1024.0;
-    float battery = analogRead(BATTERY_ADC_PIN) * 50/1024.0;
+    float solar = analogRead(SOLAR_INPUT_ADC_PIN)/1023.0 * 50.0 * SOLAR_V_CAL;
+    float battery = analogRead(BATTERY_ADC_PIN)/1023.0 * 50.0 * BATTERY_V_CAL;
 
     // Filter over time
     _solar   = _solar  *(1-FILTER_FACTOR) + solar*FILTER_FACTOR;
     _battery = _battery*(1-FILTER_FACTOR) + battery*FILTER_FACTOR;
     
-    _light0_intensity += 10;
+    _light0_intensity += LIGHT_INCREMENT;
     if (_solar > SOLAR_ACTIVE_LIMIT)    _light0_intensity = 0;
     if (_battery < BATTERY_POWER_LIMIT) _light0_intensity = 0;
 
-    if (_light0_intensity > 255) _light0_intensity=255;
+    if (_light0_intensity > MAX_LIGHT) _light0_intensity=MAX_LIGHT;
     analogWrite(LIGHT0_ENABLE_PIN, _light0_intensity);
 
     if (((_n++)&0xF) == 0)
     {
-        //Serial.print((_n++)&3);
         Serial.print(" Solar: "); Serial.print(_solar);
         Serial.print(" Battery: "); Serial.print(_battery);
         Serial.print(" Light0: "); Serial.print(_light0_intensity);
         Serial.println("");
     }
-    delay(10);
+    
+    delay(100);
 }
